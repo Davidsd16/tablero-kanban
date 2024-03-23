94a0d818a59027e96029b9ddb03ab260891e6a49
david <davidsanchezdiaz72@gmail.com>
2024-03-23 20:19:28 +0100

    Corregido error de múltiples bloques

--- src/components/TodosView.vue
+++ src/components/TodosView.vue
@@ -1,11 +1,8 @@
 <script setup>
+
     import { reactive } from 'vue'; // Importa reactive de la librería Vue 3
-    import { crypto } from 'crypto'; // Importa crypto
-</script>
 
-<!-- Este bloque contiene la lógica del componente utilizando la sintaxis <script setup> -->
-<script setup>
-
+    
     // Definición de una variable reactiva 'boards' utilizando Vue 3 Composition API
     let boards = reactive([
         // Primer objeto que representa un tablero
@@ -57,8 +54,6 @@
             ]
         }
     ]);
-
-
 </script>
 
 <!-- Este bloque contiene la estructura visual del componente -->
@@ -70,29 +65,26 @@
         </ul>
     </nav>
 
-<!-- Contenedor de tableros -->
-<div class="boards-container">
     <!-- Contenedor de tableros -->
-    <div class="boards">
-        <!-- Iteración sobre cada tablero -->
-        <div class="board" v-for="board in boards" :key="board.id">
-            <!-- Nombre del tablero -->
-            <div>{{ board.name }}</div>
-            <!-- Contenedor de ítems -->
-            <div class="items">
-                <!-- Iteración sobre cada ítem del tablero -->
-                <div class="item" v-for="item in board.items" :key="item.id">
-                    <!-- Título del ítem -->
-                    {{ item.titulo }}
+    <div class="boards-container">
+        <!-- Contenedor de tableros -->
+        <div class="boards">
+            <!-- Iteración sobre cada tablero -->
+            <div class="board" v-for="board in boards" :key="board.id">
+                <!-- Nombre del tablero -->
+                <div>{{ board.name }}</div>
+                <!-- Contenedor de ítems -->
+                <div class="items">
+                    <!-- Iteración sobre cada ítem del tablero -->
+                    <div class="item" v-for="item in board.items" :key="item.id">
+                        <!-- Título del ítem -->
+                        {{ item.titulo }}
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
-
 </template>
-
-
 
 <!-- Este bloque contiene las reglas de estilo CSS específicas del componente -->
 <style scoped></style>
