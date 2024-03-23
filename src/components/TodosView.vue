6f8cd636df66490c7a4c5b8daaa70b79645dc00c
david <davidsanchezdiaz72@gmail.com>
2024-03-23 19:23:12 +0100

    Crear tableros con sus respectivos items

--- src/components/TodosView.vue
+++ src/components/TodosView.vue
@@ -1,6 +1,59 @@
 <!-- Este bloque contiene la lógica del componente utilizando la sintaxis <script setup> -->
 <script setup>
 
+// Definición de una variable reactiva 'boards' utilizando Vue 3 Composition API
+let boards = reactive([
+    // Primer objeto que representa un tablero
+    {
+        // Generación de un ID único para el tablero utilizando la librería crypto
+        id: crypto.randomUUID(),
+        // Nombre del tablero
+        name: 'tablero 1',
+        // Lista de items en el tablero
+        items: [
+            // Primer item del tablero
+            {
+                // Generación de un ID único para el item utilizando la librería crypto
+                id: crypto.randomUUID(),
+                // Título del item
+                titulo: "Feature de archivos"
+            },
+            // Segundo item del tablero
+            {
+                // Generación de un ID único para el item utilizando la librería crypto
+                id: crypto.randomUUID(),
+                // Título del item
+                titulo: "Resolver bug"
+            }
+        ]
+    },
+    // Segundo objeto que representa otro tablero
+    {
+        // Generación de un ID único para el tablero utilizando la librería crypto
+        id: crypto.randomUUID(),
+        // Nombre del tablero
+        name: 'tablero 2',
+        // Lista de items en el tablero
+        items: [
+            // Primer item del tablero
+            {
+                // Generación de un ID único para el item utilizando la librería crypto
+                id: crypto.randomUUID(),
+                // Título del item
+                titulo: "Mandar reporte"
+            },
+            // Segundo item del tablero
+            {
+                // Generación de un ID único para el item utilizando la librería crypto
+                id: crypto.randomUUID(),
+                // Título del item
+                titulo: "Code review"
+            }
+        ]
+    }
+]);
+
+
 </script>
 
 <!-- Este bloque contiene la estructura visual del componente -->
